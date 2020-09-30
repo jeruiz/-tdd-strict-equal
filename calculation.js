226d8e6f3997f0354c29c5ef680a5c9c73738112
Jessica Ruiz <jeruiz1010@gmail.com>
2020-09-30 19:19:44 -0400

    update

--- calculation.js
+++ calculation.js
@@ -1,9 +1,5 @@
 const sumAll = (nums) => {
-  return 5;
-};
+  return 5
+}
 
-module.exports = sumAll;
-
-Object.is(1, 1); // => true
-Object.is(1, "1"); // => false
-Object.is(1, true); // => false
+module.exports = sumAll
--- tests/calculation.test.js
+++ tests/calculation.test.js
@@ -1,16 +1,16 @@
-let describe = require("mocha").describe;
-let it = require("mocha").it;
-let expect = require("chai").expect;
-let sumAll = require("../calculation");
+let describe = require('mocha').describe
+let it = require('mocha').it
+let expect = require('chai').expect
+let sumAll = require('../calculation')
 
-describe("calculation", () => {
-  describe("sumAll", () => {
-    it("returns the sum of the number in the array provided", () => {
-      let nums = [1, 2, 3, 4];
+describe('calculation', () => {
+  describe('sumAll', () => {
+    it('returns the sum of the number in the array provided', () => {
+      let nums = [1, 2, 3, 4]
 
-      let total = sumAll(nums);
+      let total = sumAll(nums)
 
-      expect(total).to.equal(5);
-    });
-  });
-});
+      expect(total).to.equal(5)
+    })
+  })
+})
